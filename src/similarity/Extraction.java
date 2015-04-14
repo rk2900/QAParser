14a2f5e8954d06713b8137a1a3ac5a74961115c6
Zhu Chenhao <zhuchenhao1992@gmail.com>
2015-04-14 11:22:18 +0800

    tmp

--- src/similarity/Extraction.java
+++ src/similarity/Extraction.java
@@ -1,5 +1,6 @@
 package similarity;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 
@@ -9,9 +10,35 @@ import finder.Pipeline;
 
 public class Extraction {
 	
-	public static void lexical(String word1, String word2){
-		
+	public static int getCommonStrLength(String str1, String str2){
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+         
+		int len1 = str1.length();  
+		int len2 = str2.length(); 
+		String min = null;  
+        String max = null;  
+        String target = null;
+        
+        min = len1 <= len2 ? str1 : str2;
+        max = len1 >  len2 ? str1 : str2;
+        
+        //最外层：min子串的长度，从最大长度开始
+        for (int i = min.length(); i >= 1; i--) {
+            //遍历长度为i的min子串，从0开始
+            for (int j = 0; j <= min.length() - i; j++) {  
+                target = min.substring(j, j + i);  
+                //遍历长度为i的max子串，判断是否与target子串相同，从0开始
+                for (int k = 0; k <= max.length() - i; k++) {  
+                    if (max.substring(k,k + i).equals(target)) {  
+                        return i;  
+                    }
+                }
+            }
+        }
+        return 0;  
 	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -34,7 +61,7 @@ public class Extraction {
 		System.out.println(q.surPredicates.size());
 		
 		for (String predict : predicts) {
-			System.out.println(predict);
+//			System.out.println(predict);
 			LinkedList<String> labels = ClientManagement.getLabel(predict);
 			StringBuilder sb = new StringBuilder();
 			for (String label : labels) {
---
+++ src/similarity/test.java
@@ -0,0 +1,11 @@
+package similarity;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l = Extraction.getCommonStrLength("vicepresident", "VicePresident");
+		System.out.println(l);
+	}
+
+}
