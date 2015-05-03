864d7f500ba59075067fa6a333f52bed68b4abe3
liuxuan <liuxuan0526@gmail.com>
2015-05-03 10:53:19 +0800

    debug result

--- src/syntacticParser/Main.java
+++ src/syntacticParser/Main.java
@@ -8,22 +8,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import baseline.Entity;
 import paser.QuestionFrame;
 import edu.stanford.nlp.trees.Tree;
+import finder.Pipeline;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new FileReader("./data/lx/all.txt"));
+		
 		BufferedWriter bw=new BufferedWriter(new FileWriter("./data/lx/199_ans_RNNchar.txt"));
-		int k=0;
-		while(true) {
-			
-			System.out.println(++k);
-			String str=br.readLine();
-			if(str==null) break;
-			str=str.split("\t")[1];
-		//	bw.write(get(str)+"\r\n");
-			
+		Pipeline pipeline=new Pipeline();
+		baseline.Main.setEntity(pipeline);
+		for (int i=1;i<=300;i++) {
+			System.out.println(i);
+			QuestionFrame qf = pipeline.xmlParser.getQuestionFrameWithPseudoId(i);
+			bw.write(get(qf,qf.question));
+			for (Entity e:qf.getEntityList()) {
+				bw.write(e.getUri()+"["+e.getStart()+","+e.getEnd()+"]\r\n");
+			}
+			bw.write("\r\n");
 		}
 		bw.close();
 	}
@@ -33,7 +36,8 @@ public class Main {
 		ret+=t.pennString();
 		ConstraintSet c=new ConstraintSet();
 		constraintExtractor.extract(qf, t, t, c.ans, c);
-		ret+=c.toString()+"\r\n";
+		c.transform(qf.wordList);
+		ret+=c.toString();
 		return ret;
 	}
 }
