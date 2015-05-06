bb25bfc0a9e0fb56ef571bec4e0da2e14fde5018
Ren Kan <rk6556@gmail.com>
2015-05-06 11:37:13 +0800

    [FIX] focusConstraint

--- src/paser/FocusConstraint.java
+++ src/paser/FocusConstraint.java
@@ -1,9 +1,11 @@
 package paser;
 
+import java.awt.List;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 
+import tool.OutputRedirector;
 import type.Type;
 import knowledgebase.ClientManagement;
 
@@ -36,25 +38,35 @@ public class FocusConstraint {
 		Pipeline pipeline = new Pipeline();
 		Main.setEntity(pipeline);
 		LinkedList<QuestionFrame> resourceQuestions = pipeline.resource;
+		OutputRedirector.openFileOutput("./data/type_of_answer_and extracted_types.txt");
 		for (QuestionFrame questionFrame : resourceQuestions) {
 			System.out.println(questionFrame.id+"\t"+questionFrame.question);
 			Focus focus = questionFrame.focus;
 			if(!focus.isEmpty() && questionFrame.answerType.equalsIgnoreCase("resource")) {
 				String focusString = focus.getFocusContent(questionFrame.wordList);
 				System.out.println("Focus: "+"\t"+focusString);
-				String typeUri = Type.getTypeFromFocus(focusString);
-				System.out.println("Type: "+"\t"+typeUri);
+				ArrayList<String> typeUriList = (ArrayList<String>)Type.getTypeFromFocus(focusString);
+				System.out.println("Type: "+"\t"+typeUriList);
 				ArrayList<String> answers = questionFrame.answers;
 				HashSet<String> typeSet = new HashSet<>();
 				for (String string : answers) {
 					typeSet.addAll(ClientManagement.getResourceType(string));
 				}
-				System.out.println("Contains? "+"\t"+typeSet.contains(typeUri));
+				Boolean containsFlag = false;
+				for (String string : typeUriList) {
+					if(typeSet.contains(string)) {
+						containsFlag = true;
+						break;
+					}
+				}
+				System.out.println("Contains? "+"\t"+containsFlag);
 				System.out.println(typeSet);
+			} else {
+				System.out.println("Answer type is not a resource.");
 			}
 			System.out.println();
 		}
-		
+		OutputRedirector.closeFileOutput();
 	}
 
 }
